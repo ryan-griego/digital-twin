79dda29e36e60118f91bc896861960289969424f
Ryan Griego <ryangriego@gmail.com>
2025-12-02 13:12:27 -0800

    Add temporary us-east-1 provider to clean up old ACM resources

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -17,3 +17,9 @@ provider "aws" {
   alias  = "us_west_2"
   region = "us-west-2"
 }
+
+# Temporary provider to clean up old resources from us-east-1
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
