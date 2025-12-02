68fa8b8d017706c87c4fc46b289b8737c56ad3db
Ryan Griego <ryangriego@gmail.com>
2025-12-02 13:04:38 -0800

    add .githubfolder and workflows

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,5 @@ terraform {
   backend "s3" {
     # These values will be set by deployment scripts
     # For local development, they can be passed via -backend-config
-    use_lockfile = true  # Replace dynamodb_table
   }
 }
